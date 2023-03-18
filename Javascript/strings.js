de43b792ccb665089101dd31407c4f42822ee24b
farocan <anna.fr.1990@gmail.com>
2023-03-18 16:49:57 +0100

    JS strings basic commands

---
+++ Javascript/strings.js
@@ -0,0 +1,7 @@
+// String methods
+let personName = "anna"
+personName.length // output: 4
+personName.toUpperCase() // output: 'ANNA'
+personName.slice(2) // output: 'na'
+personName.slice(2).toUpperCase() // output: 'NA'
+
