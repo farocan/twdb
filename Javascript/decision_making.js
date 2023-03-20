ed86256c6da10ce72f23d1a14c9eef4e30c2781f
farocan <anna.fr.1990@gmail.com>
2023-03-20 09:22:13 +0100

    decision making conditionals

---
+++ Javascript/decision_making.js
@@ -0,0 +1,25 @@
+> 1 > 3 //false
+> 1 < 3 // true
+> -1 <= -1 // true
+> -1 <= 10 //true
+> let age = 18 //undefined
+> age <= 18 //true
+> "A" < "a" //true
+> 1 == 1 // true
+> 1 == "1" // true
+> 0 == "" //true
+> 0 == false // true
+> null == undefined // true
+> 1 == 2 // false
+> "a" == "b" // false
+> 1 === 1 // true
+> 1 === "1" // false
+> 1 == "1" // true
+> 0 === false // false
+> 0 == false // true
+> 1 != 2 // true
+> 1 != "2" // true
+> 1 != "1" // false
+> 1 == "1" // true
+> 1 !== "1" // true
+> 1 === "1" // false
