a552ababf749ce3135a1d15f7a3c2d4f7cc934ad
farocan <anna.fr.1990@gmail.com>
2023-03-28 08:59:18 +0200

    Object Literals

---
+++ Javascript/Object Literals/app.js
@@ -0,0 +1,6 @@
+const person = {firstName: "Mick", lastName: "Jagger"} // undefined
+> person // { firstName: 'Mick', lastName: 'Jagger' }
+const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]} // undefined
+
+// Object Exercise 1
+const product = {name: "Gummy Bears", inStock: true, price: 1.99, flavors: ["grape", "apple", "cherry"]}
