2097b993473772bdd03cd0498a67581a2ab06b5f
farocan <anna.fr.1990@gmail.com>
2023-03-30 08:39:09 +0200

    Combining Arrays and Objects

--- Javascript/Object Literals/app.js
+++ Javascript/Object Literals/app.js
@@ -6,18 +6,18 @@ const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]}
 const product = {name: "Gummy Bears", inStock: true, price: 1.99, flavors: ["grape", "apple", "cherry"]}
 
 const nums = [23, 45, 99] // undefined
-> nums[1] // 45
-> const person = {firstName: "Mick", lastName: "Jagger"} // undefined
-> person ["firstName"] // 'Mick'
-> const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]} // undefined
-> kitcehSink ["colors"] // [ 'red', 'orange' ]
-> kitcehSink ["ggg"] // undefined
-> person.firstName // 'Mick'
+nums[1] // 45
+const person = {firstName: "Mick", lastName: "Jagger"} // undefined
+person ["firstName"] // 'Mick'
+const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]} // undefined
+kitcehSink ["colors"] // [ 'red', 'orange' ]
+kitcehSink ["ggg"] // undefined
+person.firstName // 'Mick'
 const years = {1999: "GOOD", 2020: "BAD"} // undefined
 > years // { '1999': 'GOOD', '2020': 'BAD' }
 > years ["1999"] // 'GOOD'
 > years [1999] // 'GOOD'
-> const dumbdumb = {true: "dat", null: "ffghgd"} // undefined
+const dumbdumb = {true: "dat", null: "ffghgd"} // undefined
 > dumbdumb ["true"] // 'dat'
 > dumbdumb.null // 'ffghgd'
 > person // { firstName: 'Mick', lastName: 'Jagger' }
@@ -25,10 +25,38 @@ const years = {1999: "GOOD", 2020: "BAD"} // undefined
 > person ["lastName"] // 'Jagger'
 > person [lastName] // Uncaught ReferenceError: lastName is not defined
 > person ["first" + "Name"] // 'Mick'
-> let birthyear = 2020 // undefined
-> years.birthyear // undefined
+let birthyear = 2020; // undefined
+years.birthyear // undefined
 > birthyear // 2020
 > years [birthyear] // 'BAD'
 
 // Object Access Exercise
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+let fullAddress = restaurant["address"] + ", " + restaurant ["city"]+ ", " + restaurant ["state"] + ", "+ restaurant["zipcode"] // undefined
+fullAddress // '99 Johnson Ave, Brooklyn, NY, 11206'
+
+const midterms = {danielle: 96, tomas: 78} // undefined
+midterms // { danielle: 96, tomas: 78 }
+danielle // Uncaught ReferenceError: danielle is not defined
+midterms.tomas // 78
+midterms.tomas = 79; // 79
+midterms.tomas // 79
+midterms.tomas = "C+"; // 'C+'
+midterms ["danielle"] = "A" // 'A'
+midterms // { danielle: 'A', tomas: 'C+' }
+midterms.ezra = "B+" // 'B+'
+midterms // { danielle: 'A', tomas: 'C+', ezra: 'B+' }
+midterms ["antonio"] = "A-" // 'A-'
+
+// Arrays + Objects
+const comments = [ {username: "Tammy", text: "lolololol", votes: 9}, {username: "FishBoi", text: "glub glub", votes: 47585939}] // undefined
+> comments [1] // { username: 'FishBoi', text: 'glub glub', votes: 47585939 }
+> comments [1].text // 'glub glub'
+
 
