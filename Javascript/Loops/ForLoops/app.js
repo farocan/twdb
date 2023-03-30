e65aade115646bae675508295e5ab1c170804ce2
farocan <anna.fr.1990@gmail.com>
2023-03-30 09:11:44 +0200

    Intro for Loops

---
+++ Javascript/Loops/ForLoops/app.js
@@ -0,0 +1,34 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// for (let i=1; i<=10; i++ ) {
+//     console.log(i);
+// }
+
+// // For Loop Practise 1
+// for (let i=1; i<=6; i++) {console.log("Da ba dee da ba daa");} // Da ba dee da ba daa // Da ba dee da ba daa // Da ba dee da ba daa // Da ba dee da ba daa // Da ba dee da ba daa // Da ba dee da ba daa
+
+// for (let i=1; i<=20; i+=2) {
+//     console.log(i)
+// }
+
+// for (let i=100; i>=0; i-=10) {
+//     console.log(i);
+// }
+
+for (let i=10; i<=1000; i*=10) {
+    console.log(i);
+}
+
+// More For Loops Practice
+for (let i=25; i>=0; i-=5){console.log(i);} //  25 // 20 // 15 // 10 // 5 // 0
+
+
