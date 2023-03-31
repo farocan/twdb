828fc8ea5ea7daba36ae8e7fbb82165361e3dade
farocan <anna.fr.1990@gmail.com>
2023-03-31 09:00:52 +0200

    Iterating Arrays and Nested Loops

--- Javascript/Object Literals/app.js
+++ Javascript/Object Literals/app.js
@@ -1,6 +1,6 @@
-const person = {firstName: "Mick", lastName: "Jagger"} // undefined
-> person // { firstName: 'Mick', lastName: 'Jagger' }
-const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]} // undefined
+// const person = {firstName: "Mick", lastName: "Jagger"} // undefined
+// > person // { firstName: 'Mick', lastName: 'Jagger' }
+// const kitcehSink = { favNum: 36585895, isFunny: true, colors: ["red", "orange"]} // undefined
 
 // Object Exercise 1
 const product = {name: "Gummy Bears", inStock: true, price: 1.99, flavors: ["grape", "apple", "cherry"]}
@@ -56,7 +56,7 @@ midterms ["antonio"] = "A-" // 'A-'
 
 // Arrays + Objects
 const comments = [ {username: "Tammy", text: "lolololol", votes: 9}, {username: "FishBoi", text: "glub glub", votes: 47585939}] // undefined
-> comments [1] // { username: 'FishBoi', text: 'glub glub', votes: 47585939 }
-> comments [1].text // 'glub glub'
+ comments [1] // { username: 'FishBoi', text: 'glub glub', votes: 47585939 }
+ comments [1].text // 'glub glub'
 
 
