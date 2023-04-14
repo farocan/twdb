fb17e161d45f8b3a5fc61e9d1111efa2d5f0b985
farocan <anna.fr.1990@gmail.com>
2023-04-14 08:26:54 +0200

    Guessing Game bug fixes

--- Javascript/Loops/While Loops/app.js
+++ Javascript/Loops/While Loops/app.js
@@ -25,49 +25,53 @@
 // }
 
 // Guessing Game
-// let maximum = parseInt (prompt ("Enter the maximum number!"));
-// while (!maximum) {
-//     maximum = parseInt (prompt ("Enter a valid number!"));
-// }
+let maximum = parseInt (prompt ("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt (prompt ("Enter a valid number!"));
+}
 
-// const targetNum = Math.floor(Math.random() * maximum) + 1
+const targetNum = Math.floor(Math.random() * maximum) + 1
 
-// let guess = parseInt (prompt("Enter your first guess"));
-// let attempts = 1;
+let guess = prompt("Enter your first guess! (Type 'q' to quit the game)");
+let attempts = 1;
 
-// while (parseInt(guess) !== targetNum) {
-//     if (guess=== "q") break;
-//     attempts ++;
-//     if (guess> targetNum) {
-//         guess = prompt ("Too high! Enter a new guess:");
-//     } else {
-//         guess = prompt ("Too low! Enter a new guess:");
-//     }
-// }
-// if (guess === "q") {
-//     console.log("OK, YOU QUIT!")
-// } else {
-//     console.log ("CONGRATS YOU WIN!")
-//     console.log(`You got it! It took you ${attempts} guesses`);
-// }
+while (parseInt(guess) !== targetNum) {
+    if (guess=== "q") break;
+    guess = parseInt (guess);
+    if (guess> targetNum) {
+        guess = prompt ("Too high! Enter a new guess:");
+        attempts ++;
+    } else if (guess< targetNum) {
+        guess = prompt ("Too low! Enter a new guess:");
+        attempts ++;
+    } else {
+        guess = prompt("Invalid guess. Please, enter a number or 'q' to quit.");
+    }
+}
+if (guess === "q") {
+    console.log("OK, YOU QUIT!")
+} else {
+    console.log ("CONGRATS YOU WIN!")
+    console.log(`You got it! It took you ${attempts} guesses`);
+}
 
 // The For...Of Loop
-const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
 
-for (let i=0; i< subreddits.length; i++) {
-    console.log(`Visit reddit.com/r/${subreddits[i]}`)
-}
+// for (let i=0; i< subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
 
-for (let subreddit of subreddits) {
-    console.log(`Visit reddit.com/r/${subreddit}`)
-}
+// for (let subreddit of subreddits) {
+//     console.log(`Visit reddit.com/r/${subreddit}`)
+// }
 
-// For...Of Practice 
+// // For...Of Practice 
 
-const numbers = [1,2,3,4,5,6,7,8,9]; 
-for (let square of numbers) {
-    console.log(square*square)
-}
+// const numbers = [1,2,3,4,5,6,7,8,9]; 
+// for (let square of numbers) {
+//     console.log(square*square)
+// }
 
 
 
