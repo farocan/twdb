75ff0eccd4feabb1cf7ce280e47730f4bddfd5c9
farocan <anna.fr.1990@gmail.com>
2023-04-14 09:10:40 +0200

    Intro Functions

---
+++ Javascript/Functions/app.js
@@ -0,0 +1,33 @@
+// function singSong () {
+//     console.log("DO")
+//     console.log("RE")
+//     console.log("MI")
+// }
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
+//Heart Function Exercise
+// function printHeart () {console.log("<3")}
+// printHeart ()
+
+//Arguments
+// function greet(firstName) {
+//     console.log(`Hey there, ${firstName}!`)
+// }
+
+//Rant Exercise
+function rant (message){
+    console.log (`${message.toUpperCase()}`)
+    console.log (`${message.toUpperCase()}`)
+    console.log (`${message.toUpperCase()}`)
+}
+
+rant ("I hate beets")
