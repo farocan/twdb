caa5ce14e35d16a581ba009cb6d080c33a2d50fa
farocan <anna.fr.1990@gmail.com>
2023-04-18 09:07:38 +0200

    Leveling up Functions

---
+++ Javascript/Functions/Function Expressions/app.js
@@ -0,0 +1,17 @@
+// function add (x, y){
+//     return x + y;
+// }
+
+// const add = function (x, y){
+//     return x + y;
+// }
+
+//Function Expression Exercise
+// let square = function (b){
+//     return b*b;
+// }
+// square(4) //16
+// square(3) //9
+
+//Higher Order Functions - Functions that operate on/with other functions: they accept other functions as arguments or they return a function.
+
