51275ca9050b12211d81ba967441cc962f3c8ccb
farocan <anna.fr.1990@gmail.com>
2023-04-19 21:11:28 +0200

    ForEach & Methods

---
+++ Javascript/Callback Methods/ForEach/app.js
@@ -0,0 +1,53 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+// const doubles = numbers.map(function(num){
+//     return num *2;
+// })
+
+// numbers.forEach(function(el){
+//     if (el % 2 === 0){
+//     console.log(el)
+//     }
+// })
+
+// for (let el of numbers){
+//     console.log(el);
+// }
+
+// const movies = [
+//     {
+//         title: "Amadeus",
+//         score: 99
+//     },
+//     {
+//         title: "Stand By Me",
+//         score: 85
+//     },
+//     {
+//         title: "Parasite",
+//         score: 95
+//     },
+//     {
+//         title: "Alien",
+//         score: 90
+//     }
+// ]
+
+// // movies.forEach(function(movie){
+// //     console.log(`${movie.title} - ${movie.score}/100`)
+// // })
+
+// const titles = movies.map(function(movie){
+//     return movie.title.toUpperCase();
+// })
+
+// //Map Practice
+// // DO NOT ALTER THE FOLLOWING CODE:
+// const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+// // Write your code here
+// const firstNames = fullNames.map(function(name){
+//     return name.first;
+// })
+// console.log(firstNames)
+//  ['Albus', 'Harry', 'Hermione', 'Ron', 'Rubeus', 'Minerva', 'Severus']
