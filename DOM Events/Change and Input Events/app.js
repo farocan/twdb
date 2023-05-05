49a602700e7b31c17f38693c274e9b64e2f14369
farocan <anna.fr.1990@gmail.com>
2023-05-05 07:58:23 +0200

    Input Event Practice

--- DOM Events/Change and Input Events/app.js
+++ DOM Events/Change and Input Events/app.js
@@ -9,13 +9,15 @@ input.addEventListener ("input", function(e){
     h1.innerText = input.value;
 })
 
-//Input Event Practice ***************
-// const h1 = document.querySelector("h1");
-// const input = document.querySelector("input");
+//Input Event Practice 
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
 
-// input.addEventListener("input", function(e){
-//     h1.innerText = `Hello ${input.value}`
-    
-// })
+// input.addEventListener('input', () => {
+//   if (input.value === '') {
+//     h1.textContent = 'Enter Your Username';
+//   } else {
+//     h1.textContent = `Welcome, ${input.value}`;
+//   }
+// });
 
-// ******************
