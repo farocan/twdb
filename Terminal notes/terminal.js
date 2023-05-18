a70e2bc6ec60d6afee8cd557a9252bbd8ab310c5
farocan <anna.fr.1990@gmail.com>
2023-05-18 09:16:47 +0200

    Terminal Basics

---
+++ Terminal notes/terminal.js
@@ -0,0 +1,17 @@
+// Terminal notes:
+// ls - to list all the content from the folder we are in
+// pwd- to give us the directory (where are we?)/ path to our working directory or location
+// cd - to change the directory/move to another folder from the directory we are
+// cd .. - to go back one folder
+// clear - to clear the content from the terminal
+// Absolute Paths: cd /Users/Anna/Documents - this works anywhere because it starts at the root
+// Relative Paths: cd ../../Documents (go back two levels, until Documents folder)- it works relative to where I am at currently
+// mkdir - make directory (to create a new folder)
+// mdir ../Frogs - to make a new folder in the previous folder, not where I am
+// man ls - it gives us a directory with a list of flags
+// ls -a - it gives us the given files from that folder
+// ls -l - it gives us more info about the files we have in the folder, such as modification time/last update
+// touch - to create a file. If we use touch with a file that already exists, it will update/replace it.
+// rm - to remove files. When we use this, the file won't go to the recycle bin but will be gone forever
+// rmdir - to remove folders. It only removes empty folders.
+// rm -rf - to remove a folder and everything what's inside.
