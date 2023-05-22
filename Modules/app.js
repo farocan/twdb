e9384f001be4768ca1d9995c11498b3de43ab2e5
farocan <anna.fr.1990@gmail.com>
2023-05-22 09:16:53 +0200

    Modules

---
+++ Modules/app.js
@@ -0,0 +1,7 @@
+const math = require("./math")
+
+const cats = require ("./shelter")
+
+// console.log(math.PI)
+// console.log(math.square(9))
+console.log("REQUIRED AN ENTIRE DIRECTORY", cats)
---
+++ Modules/math.js
@@ -0,0 +1,19 @@
+const add = (x, y) => x + y;
+const PI = 3.14159;
+const square = x => x * x;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+// exports.add = add;
+// exports.PI = PI;
+// exports.square = square;
+
+const math = {
+    add: add,
+    PI: PI,
+    square: square
+}
+
+module.exports= math;
