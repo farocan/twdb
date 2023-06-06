e06696c470231d16c6a50bc42c0a340515cb7bdc
farocan <anna.fr.1990@gmail.com>
2023-06-06 09:16:25 +0200

    Mongoose Schema Validations

---
+++ Mongoose/product.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+    .then(()=>{
+    console.log("CONNECTION OPEN!!!")
+ })
+ .catch(err=>{
+    console.log("OH NO ERROR!!!!!")
+    console.log(err)
+ })
+
+ const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    }
+ });
+
+ const Product = mongoose.model("Product", productSchema);
+
+ const bike = new Product ({name: "Bike Helmet", price: 19.50, categories: ["Cycling", "Safety" ]})
+ bike.save()
+ .then(data =>{
+    console.log("IT WORKED!")
+    console.log(data);
+ })
+ .catch(err => {
+    console.log("oh no error!")
+    console.log(err)
+ })
