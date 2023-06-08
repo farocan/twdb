91521e095c90574419ccfc7cd3b10cb9fc40a697
farocan <anna.fr.1990@gmail.com>
2023-06-08 09:02:40 +0200

    Mongoose with Express

---
+++ Mongoose_Express/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express ();
+const path = require("path");
+const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(()=>{
+    console.log("MONGO CONNECTION OPEN!!!")
+ })
+ .catch(err=>{
+    console.log("OH NO MONGO CONNECTION ERROR!!!!!")
+    console.log(err)
+ })
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/products", async (req, res)=> {
+    const products = await Product.find({})
+    res.render("products/index", {products})
+})
+
+app.listen(3000, ()=> {
+    console.log("APP IS LISTENING ON PORT 3000!")
+})
