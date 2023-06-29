1c7da34fae797ee054b8e447301a271dacb41f05
farocan <anna.fr.1990@gmail.com>
2023-06-29 09:27:48 +0200

    YelpCamp: Basic Styles

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async()=> {
     await Campground.deleteMany({});
     for (let i =0; i< 50; i++){
         const random1000 = Math.floor(Math.random()* 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem similique suscipit minus necessitatibus voluptatem illo fugiat excepturi earum labore obcaecati praesentium facere exercitationem, error ut laboriosam at reiciendis assumenda deserunt.',
+            price
         })
         await camp.save();
     }
