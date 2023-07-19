9560b13501ea2082941d2d816604d75803d97953
farocan <anna.fr.1990@gmail.com>
2023-07-19 19:34:45 +0200

    Registering, Login, Log out

--- Authentication/Auth/index.js
+++ Authentication/Auth/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
@@ -18,12 +19,23 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({extended: true}));
+app.use(session({ secret: 'notagoodsecret'}))
 
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id){
+        return res.redirect('/login')
+    }
+    next();
+}
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE')
 })
 
-app.get('/register', async (req, res) => {
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
     const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
     const user = new User ({
@@ -31,16 +43,38 @@ app.get('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/')
 })
 
-
-app.post('/register', async(req, res) => {
-    res.send(req.body)
+app.get('/login', (req, res) => {
+    res.render('login')
 })
 
-app.get('/secret', (req, res) => {
-    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({username});
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(validPassword){
+        req.session.user_id = user._id;
+        res.redirect('/secret')
+    } 
+    else {
+        res.redirect('/login')
+    }
+})
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret')
+})
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send("TOP SECRET!!!")
 })
 
 app.listen(3000, () => {
