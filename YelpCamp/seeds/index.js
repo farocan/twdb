dda6e8af707e78c191734d2b088753bbaddc583b
farocan <anna.fr.1990@gmail.com>
2023-07-21 09:41:27 +0200

    Basic Authorization

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async()=> {
         const random1000 = Math.floor(Math.random()* 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '64ba20b34e842698d3a651ad',
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://picsum.photos/500/400',
