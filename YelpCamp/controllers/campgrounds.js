85e0293ed7f613ea172c1d99cdd939074a0dcfd1
farocan <anna.fr.1990@gmail.com>
2023-07-27 09:25:57 +0200

    Deleting Images & Adding Thumbnail

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -47,11 +47,19 @@ module.exports.renderEditForm = async(req, res)=> {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    console.log('body', req.body);
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.image.push(...imgs);
     await campground.save();
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({$pull: {image:{filename: {$in: req.body.deleteImages }}}});
+        console.log(campground)
+    }
+    
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
