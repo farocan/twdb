1169f7a90ef0fad7ad29229173984536de61dceb
farocan <anna.fr.1990@gmail.com>
2023-08-16 21:18:46 +0200

    Slot Machine Exercise

---
+++ REACT/Slot Machine Exercise/App.jsx
@@ -0,0 +1,13 @@
+import Slots from "./Slots";
+import "./App.css";
+
+function App(){
+    return (
+        <div>
+            <Slots val1= "apple" val2="apple" val3="apple"/>
+            <Slots val1= "apple" val2="banana" val3="apple"/>
+        </div>
+    );
+}
+
+export default App;
