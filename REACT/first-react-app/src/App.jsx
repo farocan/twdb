7aada1276aca57765ea10df3c67796185c3e62d3
farocan <anna.fr.1990@gmail.com>
2023-08-16 21:22:24 +0200

    Slots Machine Exercise Fixed

--- REACT/first-react-app/src/App.jsx
+++ REACT/first-react-app/src/App.jsx
@@ -6,17 +6,21 @@ import Die from "./Die";
 import DoubleDice from "./DoubleDice";
 import Heading from "./Heading";
 import ColorList from "./ColorList";
+import Slots from "./Slots";
 
 function App() {
   return <div>
-    <Heading color="magenta" text="welcome!" fontSize="20px"/>
+        <Slots val1= "apple" val2="apple" val3="apple"/>
+        <Slots val1= "apple" val2="banana" val3="apple"/>
+  
+    {/* <Heading color="magenta" text="welcome!" fontSize="20px"/>
     <Heading color="teal" text="blah" fontSize="48px"/>
     <DoubleDice/>
     <DoubleDice/>
     <DoubleDice/>
 
     <ColorList colors={["red", "pink", "purple", "teal"]}/>
-    <ColorList colors={["olive", "orangered", "slategrey"]}/>
+    <ColorList colors={["olive", "orangered", "slategrey"]}/> */}
     {/* <ListPicker values={[1,2,3]}/>
     <ListPicker values={["a","b","c"]}/> 
     <Greeter person="Bill" from="Anna"/>
