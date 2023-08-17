799d05f33704cf8ffe5317827c3af7151678f093
farocan <anna.fr.1990@gmail.com>
2023-08-17 08:49:19 +0200

    Shopping List

--- REACT/first-react-app/src/App.jsx
+++ REACT/first-react-app/src/App.jsx
@@ -7,11 +7,21 @@ import DoubleDice from "./DoubleDice";
 import Heading from "./Heading";
 import ColorList from "./ColorList";
 import Slots from "./Slots";
+import ShoppingList from "./ShoppingList";
+
+const data = [
+  {id:1, item: "eggs", quantity: 12, completed: false},
+  {id:2, item: "milk", quantity: 1, completed: true},
+  {id:3, item: "chicken breasts", quantity: 4, completed: false},
+  {id:4, item: "carrots", quantity: 6, completed: true}
+];
 
 function App() {
-  return <div>
-        <Slots val1= "apple" val2="apple" val3="apple"/>
-        <Slots val1= "apple" val2="banana" val3="apple"/>
+  return (
+  <div>
+        <ShoppingList items={data}/>
+        {/* <Slots val1= "apple" val2="apple" val3="apple"/>
+        <Slots val1= "apple" val2="banana" val3="apple"/> */}
   
     {/* <Heading color="magenta" text="welcome!" fontSize="20px"/>
     <Heading color="teal" text="blah" fontSize="48px"/>
@@ -30,6 +40,7 @@ function App() {
     <Die />
     <Die numSides={10}/> */}
   </div>
+  )
 }
 
 export default App
