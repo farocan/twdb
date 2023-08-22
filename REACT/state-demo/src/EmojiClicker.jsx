18900e2f22fb2862d4eb357946c521dc0f23e045
farocan <anna.fr.1990@gmail.com>
2023-08-22 19:49:13 +0200

    Deletin and Updating elements

--- REACT/state-demo/src/EmojiClicker.jsx
+++ REACT/state-demo/src/EmojiClicker.jsx
@@ -1,16 +1,78 @@
-import {v4 as uuid} from "uuid";
-import {useState} from "react";
-export default function EmojiClicker(){
-    const [emojis, setEmojis] = useState([{id: uuid(), emoji: ":)"}]);
-    const addEmoji = () => {
-        setEmojis((oldEmojis) => [...oldEmojis, {id: uuid(), emoji: ":("}]);
-    };
-    return (
-        <div>
-            {emojis.map((e) => (
-            <span key={e.id} style={{fontSize: "4rem"}}>{e.emoji}</span>
-            ))}
-            <button onClick={addEmoji}>Add Emoji</button>
-        </div>
-    )
+// import {v4 as uuid} from "uuid";
+// import {useState} from "react";
+// export default function EmojiClicker(){
+//     const [emojis, setEmojis] = useState([{id: uuid(), emoji: ":)"}]);
+//     const addEmoji = () => {
+//         setEmojis((oldEmojis) => [...oldEmojis, {id: uuid(), emoji: ":("}]);
+//     };
+//     const deleteEmoji = (id) => {
+//         setEmojis((prevEmojis) => {
+//             return prevEmojis.filter((e)=> e.id !== id);
+//         });        
+//     };
+//     return (
+//         <div>
+//             {emojis.map((e) => (
+//                 <span
+//                     onCkick={() => deleteEmoji(e.id)} 
+//                     key={e.id}
+//                     style={{ fontSize: "4rem"}}
+//                 >
+//                     {e.emoji}
+//                 </span>
+//             ))}
+//             <button onClick={addEmoji}>Add Emoji</button>
+//         </div>
+//     )
+// }
+
+import { v4 as uuid } from "uuid";
+import { useState } from "react";
+
+function randomEmoji() {
+  const choices = ["😬", "😘", "😭", "🙄", "🤪", "🤢"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
+
+export default function EmojiClicker() {
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
+
+  const addEmoji = () => {
+    setEmojis((oldEmojis) => [
+      ...oldEmojis,
+      { id: uuid(), emoji: randomEmoji() },
+    ]);
+  };
+
+  const deleteEmoji = (id) => {
+    setEmojis((prevEmojis) => {
+      return prevEmojis.filter((e) => e.id !== id);
+    });
+  };
+
+  const makeEverythingAHeart = () => {
+    setEmojis((prevEmojis) => {
+      return prevEmojis.map((e) => {
+        return { ...e, emoji: "❤️" };
+      });
+    });
+  };
+
+  return (
+    <div>
+      {emojis.map((e) => (
+        <span
+          onClick={() => deleteEmoji(e.id)}
+          key={e.id}
+          style={{ fontSize: "4rem" }}
+        >
+          {e.emoji}
+        </span>
+      ))}
+      <button onClick={addEmoji}>Add Emoji</button>
+      <button onClick={makeEverythingAHeart}>Make them all hearts</button>
+    </div>
+  );
+}
+
+
