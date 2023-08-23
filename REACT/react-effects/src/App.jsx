4cfd57886a44820b116edf82146287643b904620
farocan <anna.fr.1990@gmail.com>
2023-08-23 21:57:20 +0200

    Effects

---
+++ REACT/react-effects/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './counter';
+import QuoteFetcher from './QuoteFetcher';
+import QuoteFetcherLoader from './QuoteFetcherLoader';
+import ProfileViewerWithSearch from './ProfileViewerWithSearch';
+
+function App() {
+  return (
+    <>
+      {/* <Counter/> */}
+      {/* <QuoteFetcherLoader/> */}
+      <ProfileViewerWithSearch/>
+    </>
+  )
+}
+
+export default App
